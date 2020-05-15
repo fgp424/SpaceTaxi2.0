3fd5c794c698bcd2367ec3daa96a496bf5b96609
Stefan Lancia <stefan.lancia@gmail.com>
2020-05-15 10:42:09 +0200

    Whatever

---
+++ SpaceTaxi/GameStates/GamePaused.cs
@@ -0,0 +1,108 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using DIKUArcade;
+using DIKUArcade.EventBus;
+using DIKUArcade.Timers;
+using DIKUArcade.Graphics;
+using DIKUArcade.Entities;
+using DIKUArcade.Input;
+using DIKUArcade.Math;
+using DIKUArcade.Physics;
+using DIKUArcade.State;
+using DIKUArcade.Utilities;
+using SpaceTaxi.LevelLoading;
+using SpaceTaxi.StaticObjects;
+
+
+namespace SpaceTaxi.GameStates { 
+    public class GamePaused : IGameState {
+        private static GamePaused instance = null;
+        private Entity backGroundImage;
+        private Text[] menuButtons; 
+        private int activeMenuButton = 0; 
+        private int maxMenuButtons;
+         
+/// <summary> Constructor that creates mainmenu instance if not already exisiting</summary>         
+        public static GamePaused GetInstance() { 
+            return GamePaused.instance ?? (GamePaused.instance = new GamePaused()); 
+        }
+        public void GameLoop(){
+            
+        }
+/// <summary> Method that initializes game state</summary>
+        public void InitializeGameState(){
+            backGroundImage = new Entity(
+                new StationaryShape(new Vec2F(0.0f, 0.0f), new Vec2F(1f, 1f)),
+                new ImageStride(55,ImageStride.CreateStrides(24, Path.Combine("Assets", "Images", "menu.png"))));
+
+            maxMenuButtons = 2;
+
+            menuButtons = new[]{new Text("Resume Game",new Vec2F(0.35f, 0.3f), new Vec2F(0.4f, 0.4f)),
+            new Text("Main Menu",new Vec2F(0.35f, 0.1f), new Vec2F(0.4f, 0.4f))
+            };
+            
+            menuButtons[0].SetColor(new Vec3I(0, 255, 0));
+            menuButtons[1].SetColor(new Vec3I(0, 0, 0));
+        }
+
+        public void UpdateGameLogic(){
+            
+        }
+/// <summary> Method that collects what is to be rendered in the game class</summary>
+        public void RenderState(){
+            backGroundImage.RenderEntity();
+
+            for (int i = 0; i< maxMenuButtons; i++){
+                menuButtons[i].RenderText();
+            }
+
+
+            
+        }
+/// <summary> Method that handles key events in the main menu </summary>
+/// <param name="keyValue"> What value of input key </param>
+/// <param name="keyAction"> What action of inputkey</param>
+        public void HandleKeyEvent(string keyValue, string keyAction){
+            if (keyAction == "KEY_PRESS") {
+                switch(keyValue) {
+                    case "KEY_UP":
+                        activeMenuButton = 0;
+                        menuButtons[0].SetColor(new Vec3I(0, 255, 0));
+                        menuButtons[1].SetColor(new Vec3I(0, 0, 0));                   
+                        break;
+                    case "KEY_DOWN":
+                        activeMenuButton = 1;
+                        menuButtons[0].SetColor(new Vec3I(0, 0, 0));
+                        menuButtons[1].SetColor(new Vec3I(0, 255, 0));
+                        break;
+                    case "KEY_SPACE":
+                        if (activeMenuButton == 0) {
+                            TaxiBus.GetBus().RegisterEvent(
+                            GameEventFactory<object>.CreateGameEventForAllProcessors(
+                            GameEventType.GameStateEvent, this, "CHANGE_STATE", "GAME_RESUME", ""));
+                        } else if (activeMenuButton ==1) {
+                            TaxiBus.GetBus().RegisterEvent(
+                            GameEventFactory<object>.CreateGameEventForAllProcessors(
+                            GameEventType.WindowEvent, this, "GAME_MAINMENU", "", ""));
+                        }
+                        break;
+                }
+            } else if (keyAction == "KEY_RELEASE"){ 
+                switch(keyValue) {
+                    case "KEY_UP":
+                        break;
+                    case "KEY_DOWN":
+                        break;
+                    case "KEY_SPACE":
+                        break;
+                }
+            } else {
+                throw new System.ArgumentException("KeyState unknown");
+            }
+        }
+
+
+
+    }
+}
